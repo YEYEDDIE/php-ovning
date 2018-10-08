42b7772b82f1515b9e306ffa6b71c9be6977a228
YEYEDDIE <edvard.seger@gmail.com>
2018-10-08 10:47:05 +0300

    first

---
+++ includes.php
@@ -0,0 +1,24 @@
+<?php
+$pageTitle = "PHP Uppgift ett"; 
+
+//Gör  $user variabeln tor att dett är rätt
+$user=""; 
+if ($user=="") {
+	$user="Guest";
+}else{
+	$user="User plz";
+}
+
+//text variabeln
+$text = "Bacon ipsum dolor amet short ribs turkey short loin 
+ribeye beef filet mignon ground round pork belly cow pastrami pork chop salami doner.
+
+ Sausage shank jowl picanha landjaeger jerky filet mignon salami corned beef meatball shoulder. 
+ 
+ Ball tip t-bone tenderloin corned beef ribeye ground round sirloin short loin turducken 
+ biltong turkey jerky. Chuck landjaeger tri-tip, tenderloin pork loin ground round pork chop pastrami tongue. ";
+
+
+//date funktionen i form: dd.mm.YYYY
+$date = date('d.m.y');
+?>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+//inkludera includes.php orkar ej
+require 'includes.php';
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- skriv ut $pageTitle i <title> -->
+<title><?php echo $pageTitle; ?> </title>
+</head>
+<body>
+<!-- skriv ut $pageTitle i <h1> -->
+<h1><?php echo strtoupper($pageTitle); ?></h1>
+
+<!-- skriver ut $användaren i <h3> -->
+<h3>User:<?php echo $user; ?></h3>
+
+<!-- skriv ut $text i <div id="content"> -->
+<div id="content">
+<?php echo nl2br($text); ?>
+</div>
+
+<!-- skriv ut $datum i <div id="content"> -->
+<p><small> Dagens datum: <?php echo $date; ?></small></p>
+</body>
+</html>
+
+
+
+
